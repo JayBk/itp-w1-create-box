4397e77c44df109758e8c16b4f9aeca86eaedadb
Josh Kopp <jkopp5549@gmail.com>
2016-10-25 05:41:42 +0000

    solutionay

--- create_box/main.py
+++ create_box/main.py
@@ -1,5 +1,39 @@
 """This is the entry point of the program."""
-
+#me and kevin did this together but i wanted to go through the whole process of forking it, cloning it, creating a workspace, 
 
 def create_box(height, width, character):
-    pass
+    box= ''
+    if height < 1 or width < 1:
+        return 'Error'
+    for x in range(height):
+        box+= character*width+'\n'
+    return box
+    
+"""
+#or we could have also done it like this, with a nested for loop like we learned in class.
+def create_box(height, width, character):
+    box=''
+    if height < 1 or width < 1:
+        return 'Error'
+    for x in range(height):
+        row=''
+        for i in range(width):
+            row+= character
+        box+= row +'\n'
+    return box
+"""
+
+
+def outline_box(height, width, character):
+    full_row = character * width
+    edge_row = character + ' '*(width-2) + character
+    interior = ''
+    
+    if height < 1 or width < 1:
+        return 'Error'
+    for x in range(1, height-1):
+        interior += edge_row+'\n'
+    
+    box = (full_row+'\n') + (interior) + (full_row)
+    
+    return box
